8d2061812f10cd7595c65f0652f0f32a0406b455
Sundareeshwaran D <96060751+Sundareeshwaran@users.noreply.github.com>
2024-03-29 18:09:16 +0530

    Add files via upload

---
+++ eg.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const uname = document.getElementById("uname").value;
+  const pwd = document.getElementById("pwd").value;
+
+  if (uname === "" || pwd === "") {
+      showMessage("Invalid username or password");
+  } else if (uname == "sanjay" && pwd == "sanju@") {
+      showMessage("Login successful!");
+  } else {
+    showMessage("Invalid username or password");
+  }
+});
+
+function showMessage(message) {
+  const messageElement = document.getElementById("message");
+  messageElement.innerText = message;
+}
